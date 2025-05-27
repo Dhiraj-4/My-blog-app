63cde438aa650d05a0185d143b29edd354fb25fc
Dhiraj-4 <dhirajdipaklondhe4@gmail.com>
2025-05-27 17:30:43 +0530

    .

--- src/routes/apiRoutes.js
+++ src/routes/apiRoutes.js
@@ -1,11 +1,14 @@
 import express from 'express';
 import blogRouter from './v1/blog.js';
-import authRouter from './v1/auth.js'
+import authRouter from './v1/auth.js';
+import searchRouter from './v1/search.js'
 
 const router = express.Router();
 
 router.use('/blog', blogRouter);
 
 router.use('/auth', authRouter);
+
+router.use('/search', searchRouter);
 
 export default router;
